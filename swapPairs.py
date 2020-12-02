aaf2ddc207fca42a5820e352e6b9db1c94e7573f
ENEC <cxc1357@126.com>
2020-12-02 08:24:28 +0800

    add files

--- swapPairs.py
+++ swapPairs.py
@@ -5,6 +5,7 @@ class ListNode:
         self.next = None
 
 class Solution:
+    # 递归调用函数，将问题拆解为相同的小规模子问题
     def swapPairs(self,head):
         if head is None or head.next is None:
             return head
@@ -18,13 +19,13 @@ if __name__ == "__main__":
     s = [1,2,3,4]
 
     head = ListNode(None)
-    tmp = head
+    cur = head
     for i in s:
         newNode = ListNode(i)
-        tmp.next = newNode
-        tmp = newNode
-    head = head.next
-
+        cur.next = newNode
+        cur = cur.next
+    head = head.next        
+        
     so = Solution()
     result = so.swapPairs(head)
     tmp = result
